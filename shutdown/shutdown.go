470a0719c79cc191a02be9f44aa67f60305afcbc
louieguo <369350118@qq.com>
2022-11-08 19:50:51 +0800

    add shutdown

---
+++ shutdown/shutdown.go
@@ -0,0 +1,39 @@
+package shutdown
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// Hook 优雅的关闭Hook，默认SIGINT and SIGTERM
+type Hook interface {
+	WithSignals(signals ...syscall.Signal) Hook //信号类型
+	Close(funcs ...func())
+}
+type hook struct {
+	ctx chan os.Signal
+}
+
+// 创建实例
+func NewHook() Hook {
+	hook := &hook{ctx: make(chan os.Signal, 1)}
+	return hook.WithSignals(syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGHUP)
+}
+
+func (h hook) WithSignals(signals ...syscall.Signal) Hook {
+	for _, s := range signals {
+		signal.Notify(h.ctx, s)
+	}
+	return h
+}
+
+func (h hook) Close(funcs ...func()) {
+	select {
+	case <-h.ctx:
+	}
+	signal.Stop(h.ctx)
+	for _, f := range funcs {
+		f()
+	}
+}
